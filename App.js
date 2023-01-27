287fbf48400e1ba97be84eb53874b8324adbcca1
MarcelloSA <103936996+msakal@users.noreply.github.com>
2023-01-27 09:13:52 -0300

    01-local

--- App.js
+++ App.js
@@ -1,12 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, StatusBar, Button, Platform } from 'react-native';
+
+import * as Notifications from "expo-notifications";
+import { useEffect, useState } from 'react';
+
+/* Manipulador de eventos de notificação */
+Notifications.setNotificationHandler({
+  handleNotification: async() => {
+    return {
+      shouldPlaySound: true,
+      shouldShowAlert: true,
+      shouldSetBadge: true,
+    }
+  }
+});
 
 export default function App() {
+
+  const [dados, setDados] = useState(null);
+
+  useEffect( () => {
+
+    /* Necessário para iOS */
+    async function permissoesIos() {
+      return await Notifications.requestPermissionsAsync({
+        ios: {
+          allowAlert: true,
+          allowSound: true,
+          allowBadge: true,
+          allowAnnouncements: true
+        }
+      });
+    }
+
+    permissoesIos();
+
+    /* Ouvinte de evento para as notificações recebidas, ou seja,
+    quando a notificação aparece no topo da tela do dispositivo. */
+    Notifications.addNotificationReceivedListener(notificacao => {
+      console.log(notificacao);
+    });
+    
+    /* Ouvinte de evento para as respostas dadas às notificações, ou seja,
+    quando o usuário interage (toca) na notificação. */
+    Notifications.addNotificationResponseReceivedListener(resposta => {
+      console.log(resposta.notification.request.content.data);
+      setDados(resposta.notification.request.content.data);
+    });
+  
+  }, []);
+
+  const enviarMensagem = async() => {
+    const mensagem = {
+      title: "Lembrete! 😀",
+      body: "Não se esqueça de tomar água, senão você morre 💀!",
+      data: { usuario: "Tiago 👽", cidade: "São Paulo" },
+      sound: Platform.OS === 'ios' ? "default" : "", // necessário pro iOS
+    };
+
+    /* Função de agendamento de notificações */
+    await Notifications.scheduleNotificationAsync({
+      content: mensagem,
+      trigger: { seconds: 5 }
+    });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <SafeAreaView style={styles.container}>
+        <Text>Exemplo de sistema de notificação local</Text>
+        <Button title='Disparar notificação' onPress={enviarMensagem} />
+
+        { dados && 
+          <View style={styles.conteudo}>
+            <Text> {dados.usuario} </Text>
+            <Text> {dados.cidade} </Text>
+          </View>
+        }
+
+      </SafeAreaView>
+    </>
   );
 }
 
@@ -17,4 +90,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  conteudo: {
+    marginVertical: 8,
+    backgroundColor: "yellow",
+    padding: 8
+  }
 });
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# notificacoes
- 
+# Notificações
+
+Sistema que permite o envio de notificações agendadas e externas mesmo com o aplicativo não estando em execução em primeiro plano no dispositivo.
+
+## Referências
+
+https://docs.expo.dev/versions/latest/sdk/notifications/
+
+https://docs.expo.dev/push-notifications/overview/
+
+## Instalação
+
+`npx expo install expo-notifications`
